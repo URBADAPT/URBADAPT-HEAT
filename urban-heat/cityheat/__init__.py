55fdf0850f09a7d9597b6ff351cf7ca164bc9f38
ArmandeAM <223158@students.hertie-school.org>
2025-12-08 18:23:07 +0100

    CleanVersionsCityAgnostic_RomeEx

--- urban-heat/cityheat/__init__.py
+++ urban-heat/cityheat/__init__.py
@@ -2,3 +2,5 @@
 __all__ = ["nbsetup"]
 from . import nbsetup
 
+# vulnerability layer 
+from .vulnerability_layer import build_vulnerability_layer, load_vulnerability, weight_exposure_by_vulnerability
